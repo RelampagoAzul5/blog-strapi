7922ff2836418fa87a4626834a8430a05e3454dc
Giovanni Henrique <giovannihpf@gmail.com>
2025-04-17 19:04:28 -0300

    Alterando o controller para buscar o post pelo ID e não pelo DocumentId

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,28 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params; // Pegando o 'id' da URL da requisição
+
+    // Verifica se o 'id' foi passado
+    if (!id) {
+      return ctx.badRequest('ID is required');
+    }
+
+    try {
+      // Utiliza o 'id' para buscar o post no banco
+      const post = await strapi.db.query('api::post.post').findOne({
+        where: { id },
+      });
+
+      if (!post) {
+        return ctx.notFound('Post not found');
+      }
+
+      return ctx.send(post);
+    } catch (error) {
+      return ctx.internalServerError('Something went wrong');
+    }
+  },
+}));
